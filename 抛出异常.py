4dcbf660ff4dd23e2c97b612ec3b4d7698f5715c
TechDP_MegaHunt <1095995709@qq.com>
2019-06-22 13:46:30 +0800

    学习抛出异常

---
+++ 抛出异常.py
@@ -0,0 +1,9 @@
+try:
+    s = input('please enter two numbers separated by comma: ')
+    num1 = int(s.split(',')[0].strip())
+    num2 = int(s.split(',')[1].strip())
+except (ValueError, IndexError) as err:
+    print('Value Error: {}'.format(err))
+    
+
+print('continue')
